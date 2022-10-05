45fe6f4d48eb3ffdea32075f10035866434d8640
DrKleiner-ops <vlad_san98@mail.ru>
2022-10-05 21:14:37 +0300

    JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        Util.createTable();
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User("Jora", "Vareny", (byte) 23));
+        users.add(new User("Perqw", "S", (byte) 65));
+        users.add(new User("Jfdsf", "Vrewr", (byte) 212));
+        users.add(new User("Jfda", "EWfdsf", (byte) 1));
+        for (User us : users) {
+            Util.addUser(us);
+            System.out.println(us.getName() + " done");
+        }
+        Util.selectAll();
+        Util.drop();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,49 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+
+
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+	private static final Util util = new Util("jdbc:postgresql://localhost:5432/test", "Test", "123");
+	private static Connection connection;
+
+	public Util(String dBUrl, String usr, String pass) {
+		try {
+			connection = DriverManager.getConnection(dBUrl, usr, pass);
+		} catch (SQLException ignored) {
+		}
+	}
+	private static void executeUpdate(String query) throws SQLException {
+		Statement statement = connection.createStatement();
+		statement.executeUpdate(query);
+	}
+	public static void createTable () throws SQLException {
+		String query = "CREATE TABLE users " +
+				"(name TEXT, lastName TEXT, age INTEGER)";
+		executeUpdate(query);
+	}
+	public static void addUser(User user) throws SQLException {
+		String query = "INSERT INTO users VALUES ('" +
+				user.getName() + "', '"	+
+				user.getLastName() + "', " +
+				user.getAge().intValue()+")";
+		executeUpdate(query);
+	}
+	public static void selectAll() throws SQLException {
+		Statement statement = connection.createStatement();
+		ResultSet result = statement.executeQuery(
+				"SELECT * FROM users");
+		while (result.next()) {
+			String name =  result.getString("name");
+			String lastName =  result.getString("lastName");
+			String age =  result.getString("age");
+			System.out.println("|" + name + "|" + lastName + "|" + age);
+		}
+	}
+	public static void drop() throws SQLException {
+		String query = "DROP TABLE users";
+		executeUpdate(query);
+	}
 }
