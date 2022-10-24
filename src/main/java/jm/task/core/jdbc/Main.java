278abafa0531c84e3c0ed0173f6bb90055b79d10
DrKleiner-ops <vlad_san98@mail.ru>
2022-10-24 18:28:35 +0300

    JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,6 +2,7 @@ package jm.task.core.jdbc;
 
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 import java.sql.SQLException;
@@ -9,17 +10,22 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Util.createTable();
+        //Util.createTable();
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
         ArrayList<User> users = new ArrayList<>();
         users.add(new User("Jora", "Vareny", (byte) 23));
         users.add(new User("Perqw", "S", (byte) 65));
         users.add(new User("Jfdsf", "Vrewr", (byte) 212));
         users.add(new User("Jfda", "EWfdsf", (byte) 1));
         for (User us : users) {
-            Util.addUser(us);
+            userService.saveUser(us.getName(),us.getLastName(),us.getAge());
             System.out.println(us.getName() + " done");
         }
-        Util.selectAll();
-        Util.drop();
+        userService.getAllUsers();
+        userService.removeUserById(3);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -2,30 +2,69 @@ package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
-public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
+public class UserServiceImpl extends Util implements UserService {
+
+
+    public UserServiceImpl() {
 
     }
 
-    public void dropUsersTable() {
+    public void createUsersTable() throws SQLException {
+        String query = "CREATE TABLE users " +
+                "(id SERIAL PRIMARY KEY, name TEXT, lastName TEXT, age INTEGER)";
+        executeUpdate(query);
 
     }
 
-    public void saveUser(String name, String lastName, byte age) {
-
+    public void dropUsersTable() throws SQLException {
+        String query = "DROP TABLE users";
+        executeUpdate(query);
     }
 
-    public void removeUserById(long id) {
-
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
+        String query = "INSERT INTO users VALUES (DEFAULT, '" +
+                name + "', '"	+
+                lastName + "', " +
+                (int) age +")";
+        executeUpdate(query);
     }
 
-    public List<User> getAllUsers() {
+    public void removeUserById(long id) throws SQLException {
+        String query = "DELETE FROM users WHERE id = " + id;
+        executeUpdate(query);
+    }
+
+    public List<User> getAllUsers() throws SQLException {
+        Statement statement = Util.getConnection().createStatement();
+        ResultSet result = statement.executeQuery(
+                "SELECT * FROM users");
+        while (result.next()) {
+            int id = result.getInt("id");
+            String name =  result.getString("name");
+            String lastName =  result.getString("lastName");
+            String age =  result.getString("age");
+            System.out.println(id + "|" + name + "|" + lastName + "|" + age);
+        }
         return null;
     }
 
-    public void cleanUsersTable() {
+    public void cleanUsersTable() throws SQLException {
+        String query = "TRUNCATE TABLE users";
+        executeUpdate(query);
+    }
 
+    private static void executeUpdate(String query) throws SQLException {
+
+        Statement statement = Util.getConnection().createStatement();
+        statement.executeUpdate(query);
     }
 }
